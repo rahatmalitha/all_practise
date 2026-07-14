0565bc9bff405d66095cb80f63925e61dd56794a
rahatmalitha <dev.mdrahat@gmail.com>
2026-07-14 10:03:40 +0600

    decimal to binary

---
+++ cpp/index.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int decToBinary(int n)
+{
+    int ans = 0, pow = 1;
+
+    while (n > 0)
+    {
+        int rem = n % 2;
+        n /= 2;
+        ans += rem * pow;
+        pow *= 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int decNum = 9;
+    int binaryNum = decToBinary(decNum);
+    cout << "Decimal: " << decNum << " => Binary: " << binaryNum << endl;
+    return 0;
+}
