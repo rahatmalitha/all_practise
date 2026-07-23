ba16a0f4befa00a929f9503f0e147a74c4af0366
rahatmalitha <dev.mdrahat@gmail.com>
2026-07-23 14:19:00 +0600

    python class

--- python/main.py
+++ python/main.py
@@ -1,7 +1,13 @@
-class Student:
-    def __init__(self, name):
-        self.name = name
-s1 = Student("Rahat")
-print(s1.name)
-del s1.name
-print(s1.name)
+PI = 22/7
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return PI * self.radius ** 2
+    def perimeter(self):
+        return 2 * PI * self.radius
+
+c1 = Circle(21)
+print(c1.area())
+print(c1.perimeter())
